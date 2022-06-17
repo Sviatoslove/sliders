c797dfb7b8c40bfa8670a60146fad782beaa1b54
Sviatoslove <svyatpopov86@gmail.com>
2022-06-17 18:26:17 +0300

    firstcommit

---
+++ main11.js
@@ -0,0 +1,63 @@
+var prev = document.getElementById('btn-prev');
+var next = document.getElementById('btn-next');
+var slides = document.querySelectorAll('.slide');
+var dots = document.querySelectorAll('.dot');
+var index = 0;
+
+function activeSlide(n) {
+  for(slide of slides) {
+    slide.classList.remove('active');
+  };
+  slides[n].classList.add('active');
+  var activeSlideData = slides[n].dataset.slide;
+  dots.forEach(function(dot, idx) {
+    dots[idx].classList.remove('active');
+    if(dots[idx].dataset.dot === activeSlideData) {
+      dots[idx].classList.add('active')
+    };
+  });
+};
+
+function dotsSlide() {
+  dots.forEach(function(dot, idx){
+    dots[idx].addEventListener('click', function(event){
+      var currTab = event.target.dataset.dot;
+      changeClass(event.target)
+      for(var i = 0; i < slides.length; i++){
+        slides[i].classList.remove('active');
+        if(slides[i].dataset.slide === currTab) {
+          slides[i].classList.add('active');
+        };
+      };
+    });
+  });
+};
+
+function changeClass(el) {
+  for(var i = 0; i < dots.length; i++){
+    dots[i].classList.remove('active');
+  };
+  el.classList.add('active');
+};
+
+function nextSlide() {
+  if(index == slides.length - 1) {
+    index = slides.length - 1;
+  } else {
+    index++;
+    activeSlide(index);
+  };
+};
+
+function prevSlide() {
+  if(index == 0) {
+    index = 0;
+  } else {
+    index--;
+    activeSlide(index);
+  };
+};
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
+dotsSlide();
